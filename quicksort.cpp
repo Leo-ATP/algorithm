0616f2800c43505f8de25d5a6b3bce907b0b8aa3
Leo-ATP <cogito.wu@gmail.com>
2018-04-06 19:52:29 -0700

    quicksort

---
+++ quicksort.cpp
@@ -0,0 +1,37 @@
+
+#include "quicksort.h"
+using namespace std;
+
+void quicksort(vector<int> &v, int st,int ed){
+    if(st<ed){
+        int n = partition(v,st,ed);
+        quicksort(v,st,n-1);
+        quicksort(v,n+1,ed);
+    }
+}
+
+int partition(vector<int> &v, int st, int ed){
+    int i= st-1;
+    int p=v[ed];
+    for(int j=st;j<ed;j++){
+        if(v[j]<p){
+            i++;
+            swap(v[i],v[j]);
+        }
+    }
+    swap(v[i+1],v[ed]);
+    return i+1;
+}
+
+
+int main(){
+
+    int itt[]= {6,3,4,1};
+    vector<int> v(itt, itt+sizeof(itt)/sizeof(int));
+    cout<<v.size()<<endl;
+    quicksort(v,0,v.size()-1);
+
+    for(auto i : v)
+        cout<<i<<endl;
+
+}
---
+++ quicksort.h
@@ -0,0 +1,9 @@
+#ifndef QUCIKSORTZ_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int> &v, int st, int ed);
+void quicksort(vector<int> &v, int st,int ed);
+#endif
